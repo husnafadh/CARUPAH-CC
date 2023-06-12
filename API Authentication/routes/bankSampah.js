49c66608c46ca9a5195c1798cd88be6b01e4c767
dellips <100208710+dellips@users.noreply.github.com>
2023-06-12 14:30:50 +0700

    Update and rename bankSampahUser.js to bankSampah.js

--- API Authentication/routes/bankSampah.js
+++ API Authentication/routes/bankSampah.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controller/bankSampahUser');
+const userController = require('../handler/bankSampahHandler');
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
