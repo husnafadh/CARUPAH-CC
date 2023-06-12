4ec6b59bf6866957cc5db104492fb4daff893e5f
Husna Fadillah <108561707+husnafadh@users.noreply.github.com>
2023-06-12 12:01:53 +0700

    Update server.js
    split the code into another file

--- API Authentication/server.js
+++ API Authentication/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const cors = require('cors');
+const authRoutes = require('../routes/auth');
 const db = require('./services/firebaseAdmin');
 const updateProfileRoute = require('./routes/updateProfile');
 
@@ -8,51 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//Registration endpoint
-app.post('/register', async (req, res) => {
-    const { nama, email, password, noTelepon } = req.body;
-    try {
-        const userRecord = await admin.auth().createUser({ email, password });
-        //Simpan data tambahan ke Firestore
-        await firestore.collection('users').doc(userRecord.uid).set({
-            nama,
-            noTelepon
-        });
-        res.json({ message: 'User registered successfully', data: userRecord.toJSON() });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
-
-// Login endpoint
-app.post('/login', async (req, res) =>{
-    const {email, password} = req.body;
-    try{
-        const userRecord = await admin.auth().getUserByEmail(email);
-        const token = await admin.auth().createCustomToken(userRecord.uid);
-        res.json({ message: 'Login successful', token});
-    } catch (error){
-        res.status(400).json({ error: error.message });
-    }
-});
-// Google OAuth sign-in endpoint
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client('1077678923255-vjvgc5q5licb5ddr5o0r9s83691704ov.apps.googleusercontent.com');
-app.post('/google-signin', async (req, res)=>{
-    const { idToken } = req.body;
-    try{
-        const ticket = await client.verifyIdToken({
-            idToken,
-            audience: '1077678923255-vjvgc5q5licb5ddr5o0r9s83691704ov.apps.googleusercontent.com',
-        });
-        const {email} = ticket.getPayload();
-        const userRecord = await admin.auth().getUserByEmail(email);
-        const token = await admin.auth().createCustomToken(userRecord.uid);
-        res.json({ message: 'Google sign-in successful', token});
-    } catch (error){
-        res.status(400).json({ error:error.message});
-    }
-});
+//authentication
+app.use('/auth', auth);
 
 //update profile
 app.use('/profile', updateProfileRoute);
