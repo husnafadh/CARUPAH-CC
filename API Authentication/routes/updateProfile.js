b61a312d36e06669542b05f5bd3445a2c4bed322
dellips <100208710+dellips@users.noreply.github.com>
2023-06-12 14:17:44 +0700

    Update updateProfile.js

--- API Authentication/routes/updateProfile.js
+++ API Authentication/routes/updateProfile.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const router = express.Router();
-// const db = require('../services/firebaseAdmin');
-const updateProfileHandler = require('../handler/updateProfileHandler')
-
-router.put('/profile/:userId', async (req, res) => {
-  const { userId } = req.params;
-  const { name, telp, city, district, subdistrict, address } = req.body;
-
-  try {
-    const result = await updateProfileHandler(userId, name, telp, city, district, subdistrict, address);
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+const userController = require('../controller/user');
+const multer = require('multer');
+const upload = multer({ dest: 'profileImages/',
+fileFilter: (req, file, cb) => {
+    // Check if the uploaded file is an image
+    if (
+        file.mimetype.startsWith('image/png') ||
+        file.mimetype.startsWith('image/jpeg') ||
+        file.mimetype.startsWith('image/jpg')
+    ) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only image files are allowed.'), false);
+    }
+  } 
 });
+
+// get all users
+router.get('/', userController.getAllUsers);
+
+// get users by id
+router.get('/:userId', userController.getAllUsersById);
+
+// update profile
+router.patch('/:userId', userController.updateProfile);
+
+// delete
+router.delete('/:userId', userController.deleteUser);
+
+// profile picture
+router.patch('/profile-picture/:userId', upload.single('image'), userController.changeProfilePicture);
+
+// change password
+router.patch('/change-password/:userId', userController.changePassword);
 
 module.exports = router;
