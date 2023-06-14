7ab86a0ef7ebf8f1bdab0564d59d22e21e932652
Husnafr <husnafadillah11@gmail.com>
2023-06-14 15:37:30 +0700

    Update File
    Fix the file

---
+++ routes/updateProfile.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const profileHandler = require('../../handler/updateProfileHandler');
+const multer = require('multer');
+const upload = multer({ dest: 'profileImages/',
+fileFilter: (req, file, cb) => {
+    // Check if the uploaded file is an image
+    if (
+        file.mimetype.startsWith('image/png') ||
+        file.mimetype.startsWith('image/jpeg') ||
+        file.mimetype.startsWith('image/jpg')
+    ) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only image files are allowed.'), false);
+    }
+  } 
+});
+
+// get all users
+router.get('/', profileHandler.getAllUsers);
+
+// get users by id
+router.get('/:userId', profileHandler.getAllUsersById);
+
+// update profile
+router.patch('/:userId', profileHandler.updateProfile);
+
+// delete
+router.delete('/:userId', profileHandler.deleteUser);
+
+// profile picture
+router.patch('/profile-picture/:userId', upload.single('image'), profileHandler.changeProfilePicture);
+
+// change password
+router.patch('/change-password/:userId', profileHandler.changePassword);
+
+module.exports = router;
