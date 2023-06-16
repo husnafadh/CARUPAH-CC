62401f821fae4bc37c05d621a3aa4f2a956f9475
dellips <100208710+dellips@users.noreply.github.com>
2023-06-16 13:31:30 +0700

    Add files via upload

---
+++ routes/artikel.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const articleController = require('../controller/artikel');
+
+// get all artikel
+router.get('/', articleController.getAllArticle);
+
+// get artikel by tag
+router.get('/:tag', articleController.getAllArticlesByTag);
+
+// create artikel
+router.post('/', articleController.createNewArticle);
+
+// update artikel
+router.patch('/:id', articleController.updateArticle);
+
+// delete
+router.delete('/:id', articleController.deleteArticle);
+
+module.exports = router;
